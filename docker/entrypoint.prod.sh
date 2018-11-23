a55996e199968c5aecd08460a7f9a988c83bf7ee
Felipe Gonçalves dos Reis <feelipereis7@gmail.com>
2018-11-23 09:34:12 -0200

    Update entrypoint.prod.sh

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 
+if [ ! -f storage/.initialized ]; then
+    touch storage/.initialized;
+    # laravel storage folder structure (v5.4+)
+    mkdir -p storage/{app/public,framework/{cache,sessions,testing,views},logs}
+    chown -R nginx:nginx storage/*
+    chmod -R ug+rwx storage/*
+fi
+
 su-exec nginx:nginx php artisan config:cache
 
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
